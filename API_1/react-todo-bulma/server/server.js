b33c8c80726b7fe329128c044da80491e5e97f25
Luan Alves <luan.alves@comunike.me>
2019-06-05 14:35:56 -0300

    feat: Rest no Postman

--- API_1/react-todo-bulma/server/server.js
+++ API_1/react-todo-bulma/server/server.js
@@ -4,19 +4,24 @@ const app = express()
 const TaskRoutes = require('./routes/Task')
 const mongoose = require('mongoose')
 const config = require('./config/keys.config.js')
+const cors = require('cors')
 
-app.use(bodyParser.json())
-app.use('/api/task', TaskRoutes)
+mongoose.connect(config.mongodbUri, { useNewUrlParser: true })
+  .then(() => console.log('Database connect'))
+  .catch((err) => console.log('Error on database connection', err))
+mongoose.set('useFindAndModify', false)
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/tasks', TaskRoutes)
 const port = process.env.PORT || 3001;
 
 app.listen(port, function(){
   console.log('Server listening on port ' + port);
 });
 
-mongoose.connect(config.mongodbUri, {useNewUrlParser: true})
-.then(() => console.log('Database connect'))
-.catch((err) => console.log('Error on database connection', err))
-mongoose.set('useFindAndModify', false)
+
 
 
 
