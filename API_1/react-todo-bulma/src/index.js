4c18575fc0054ded437cbcb9e24f05d998017389
Luan Alves <luan.alves@comunike.me>
2019-06-05 15:57:25 -0300

    Criando componentes

--- API_1/react-todo-bulma/src/index.js
+++ API_1/react-todo-bulma/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
+import Layout from './pages/Layout';
 
 render(
-  <h1>Começo da nossa aplicação React!</h1>,
-  document.getElementById('root')
+    <Layout />,
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
