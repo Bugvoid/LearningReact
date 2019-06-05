23a32d893351f0a731aeeb490eb99ae19f06b0b6
Luan Alves <luan.alves@comunike.me>
2019-06-05 14:06:44 -0300

    Criando Conexao com mongoDB

--- API_1/react-todo-bulma/server/server.js
+++ API_1/react-todo-bulma/server/server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
+const TaskRoutes = require('./routes/Task')
+const mongoose = require('mongoose')
+const config = require('./config/keys.config.js')
 
 app.use(bodyParser.json())
-
+app.use('/api/task', TaskRoutes)
 const port = process.env.PORT || 3001;
 
 app.listen(port, function(){
   console.log('Server listening on port ' + port);
 });
+
+mongoose.connect(config.mongodbUri, {useNewUrlParser: true})
+.then(() => console.log('Database connect'))
+.catch((err) => console.log('Error on database connection', err))
+mongoose.set('useFindAndModify', false)
+
+
+
 
